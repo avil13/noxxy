506202cc2c08b560670e235d3687a5feb18c2490
avil13 <13.avil@gmail.com>
2018-06-04 19:19:12 +0300

    wip

--- noxxy.js
+++ noxxy.js
@@ -2,32 +2,32 @@
 
 /*jshint strict:false*/
 require('colors');
-"use strict";
+const url = require('url');
+('use strict');
 
 var http = require('http'),
     fs = require('fs'),
     httpProxy = require('http-proxy'),
     stripJson = require('strip-json-comments'),
     program = require('commander')
-    .version(require("./package.json").version)
-    .option('-c, --config [configurationFile]', 'Set the configuration to use', false)
-    .parse(process.argv)
+        .version(require('./package.json').version)
+        .option('-c, --config [configurationFile]', 'Set the configuration to use', false)
+        .parse(process.argv),
     /**
      * load the given configuration path
      * @param {string} configPath
      */
-    ,
     getConfig = function(configPath) {
         return new Promise(function(resolve, reject) {
             fs.readFile(configPath, 'utf-8', function(err, data) {
                 if (err) {
-                    reject({ title: "Configuration file opening error", err: err, data: { file: configPath } });
+                    reject({ title: 'Configuration file opening error', err: err, data: { file: configPath } });
                 } else {
                     var config = {};
                     try {
                         config = JSON.parse(stripJson(data));
                     } catch (error) {
-                        reject({ title: "Configuration JSON  error", err: error, data: { file: configPath } });
+                        reject({ title: 'Configuration JSON  error', err: error, data: { file: configPath } });
                         return;
                     }
                     resolve(config);
@@ -39,36 +39,44 @@ var http = require('http'),
 /**
  * Main :
  */
-if (!program.config) { console.log('You need at least to give a config file'.red);
+if (!program.config) {
+    console.log('You need at least to give a config file'.red);
+    console.log('example ' + 'node noxxy,js -c config.json '.blue);
     console.log('See --help to know how');
-    process.exit(1); }
+    process.exit(1);
+}
 
 getConfig(program.config)
     .then(function(config) {
-        // console.log(config);
-
         // Create a proxy server with custom application logic
-        var proxy = httpProxy.createProxyServer({ ws: true })
-            .on('error', function(err) { console.log("Proxy Error : ".red, err); });
+        var proxy = httpProxy.createProxyServer({ ws: true }).on('error', function(err) {
+            console.log('Proxy Error : '.red, err);
+        });
         // Create a main server
-        http.createServer(function(req, res) {
+        http
+            .createServer(function(req, res) {
                 try {
                     const host = req.headers.host;
                     const domain = parseRequest(config, req);
 
-                    console.log('Web'.bgWhite.blue, host.grey, domain.target.green, req.url);
                     proxy.web(req, res, { target: domain.target });
+                    console.log('Web'.bgWhite.blue, host.grey, domain.target.green + '' + req.url);
+                    // 
+                    proxy.on('proxyReq', function(proxyReq, req, res, options) {
+                        const _u = url.parse(domain.target);
+                        proxyReq.setHeader('Host', _u.host);
+                    });
                 } catch (err) {
                     console.log('Proxy Error'.red, err);
                 }
             })
-            .on("upgrade", function(req, socket, head) {
+            .on('upgrade', function(req, socket, head) {
                 try {
                     const host = req.headers.host;
                     const domain = parseRequest(config, req);
 
-                    console.log('WebSocket'.bgWhite.green, host.grey, domain.target.green);
                     proxy.ws(req, socket, head, { target: domain.target });
+                    console.log('WebSocket'.bgWhite.green, host.grey, domain.target.green);
                 } catch (err) {
                     console.log('Proxy Error'.red, err);
                 }
@@ -77,29 +85,33 @@ getConfig(program.config)
             // 	proxyReq.setHeader('Via', 'Noxy '.program.version);
             // })
             .listen(config.server.port, config.server.hostname);
-        console.log('NOXY Server running on'.green, (config.server.hostname ? '' + config.server.hostname : '*').magenta + ':' + ('' + config.server.port).cyan);
+        console.log('NOXXY Server running on'.green, (config.server.hostname ? '' + config.server.hostname : '*').magenta + ':' + ('' + config.server.port).cyan);
     })
     .catch(function(err) {
-        console.log("Error".red, err);
+        console.log('Error'.red, err);
     });
 
-
+let host_lists = [];
+let host_lists_patterns = [];
 
 function parseRequest(config, req) {
     let domain = config.default;
-    const hostLists = Object.keys(config.domains);
-    
-    for (let i=0; i < hostLists.length; i++) {
-        let pattern = new RegExp(hostLists[i], 'i');
-        if (pattern.test(req.headers.host + req.url)) {
-            domain = config.domains[hostLists[i]];
+
+    if (host_lists.length === 0) {
+        host_lists = Object.keys(config.domains);
+        host_lists_patterns = host_lists.map(v => new RegExp(v, 'i'));
+    }
+
+    for (let i = 0; i < host_lists.length; i++) {
+        if (host_lists_patterns[i].test(req.headers.host + req.url)) {
+            domain = config.domains[host_lists[i]];
             break;
         }
     }
 
-    if (domain.rm !== undefined) {
-        req.url = req.url.replace(domain.rm, "");
-    }
+    // if (domain.rm !== undefined) {
+    //     req.url = req.url.replace(domain.rm, "");
+    // }
 
     return domain;
 }
